6c4d4464a430558d24ec09bf11fb0d96737a5a90
Alejandro Tomasella <aletomasella09@gmail.com>
2022-06-19 00:00:53 -0300

    Working on Guesses

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -7,12 +7,53 @@ const useWordle = (solution) => {
   const [history, setHistory] = useState([]);
   const [isCorrect, setIsCorrect] = useState(false);
 
-  const formatGuess = () => {};
+  const formatGuess = () => {
+    const arraySolution = solution.toUpperCase().split("");
+    const formattedGuess = currentGuess
+      .toUpperCase()
+      .split("")
+      .map((letter) => {
+        return { key: letter, color: "grey" };
+      });
 
-  const addNewGuess = () => {};
+    formattedGuess.forEach((el, i) => {
+      console.log(el);
+      console.log(i);
+      if (arraySolution[i] === el.key) {
+        el.color = "green";
+        return;
+      }
+      if (arraySolution.includes(el.key)) {
+        el.color = "yellow";
+      }
+    });
+    console.log(formattedGuess);
+  };
+
+  const addNewGuess = () => {
+    setHistory((prev) => [...prev, currentGuess]);
+    setGuesses((prev) => [...prev, currentGuess.split("")]);
+    console.log(guesses);
+    setCurrentGuess("");
+  };
 
   const handleKey = ({ key }) => {
-    console.log(key);
+    if (key === "Enter") {
+      if (turn > 5) {
+        alert("You lost");
+        return;
+      }
+      if (history.includes(currentGuess)) {
+        alert("You already tried that word.");
+        return;
+      }
+      if (currentGuess.length !== solution.length) {
+        alert(`Word must be ${solution.length} long.`);
+        return;
+      }
+      formatGuess();
+    }
+    // console.log(key);
     if (key === "Backspace") {
       setCurrentGuess((prev) => prev.slice(0, -1));
       return;
